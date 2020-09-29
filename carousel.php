18154da97e2a529f68fc6c91cd4bf1bd2bba69a1
alissonrgalindo <alissonricardogalindo@gmail.com>
2020-09-29 16:39:14 -0300

    Now is PHP

---
+++ carousel.php
@@ -0,0 +1,50 @@
+<!-- Carousel  ======================================================= -->
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/banner-home/banner-1.jpg" class="banner">
+      <div class="carousel-caption align-self-baseline">
+        <div class="row h-100 align-items-center justify-content-center text-center">
+          <div class="align-self-end">
+            <h1 class="text-white font-weight-medium">Queres ser voluntário?</h1>
+            <p class="text-white font-weight-normal text-banner mb-5">Se gosta de animais, tens sentido de responsabilidade e estás disposto a ajudar
+              então entra em contacto connosco.</p>
+            <a class="js-scroll-trigger btn btn_banner" href="help.html">QUERO AJUDAR</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/banner-home/banner-2.jpg" class="banner">
+      <div class="carousel-caption">
+        <div class="row h-100 align-items-center justify-content-center text-center">
+          <div class="align-self-end align-self-baseline">
+            <h1 class="text-white font-weight-medium">Ajude-nos a ajudar</h1>
+            <p class="text-white font-weight-normal text-banner mb-5">Não recebemos qualquer tipo de ajuda institucional,
+              por isso todas as ajudas são muito importantes. E há varias formas de ajudar. Basta querer</p>
+            <a class="js-scroll-trigger btn btn_banner" href="help.html">QUERO AJUDAR</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/banner-home/banner-3.jpg" class="banner">
+      <div class="carousel-caption">
+        <div class="row h-100 align-items-center justify-content-center text-center">
+          <div class="align-self-end align-self-baseline">
+            <h1 class="text-white font-weight-medium">Adote um animal</h1>
+            <p class="text-white font-weight-normal text-banner mb-5">Adotar um animal é um ato de amor!
+              Conheça nossos patutos disponíveis.</p>
+            <a class="js-scroll-trigger btn btn_banner" href="adopt.html">ANIMAIS PARA ADOÇÃO</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
---
+++ footer.php
@@ -0,0 +1,49 @@
+<section class="contact-wrap container width--forced" id="contact">
+  <div class="container">
+    <div class="row row--footer row--column">
+      <div class="box-footer">
+        <img class="logo-footer" src="./img/logo/LogoStreetdogs@2x.jpg">
+      </div>
+      <div class="box-footer">              
+        <h3 class="contact_social">A ASSOCIAÇÃO</h3>
+        <div class="text-footer">
+          <img class="icon-footer size-icon" src="./img/icon/map-marker-alt-solid-2.svg" alt="ícone de mapa">Barcelos, Portugal
+        </div>
+        <div class="text-footer">
+          <a href="mailto:geral@streetdogs.pt" target="_blank"><img alt="ícone de email" src="./img/icon/envelope-regular.svg" class="icon-footer size-icon">geral@streetdogs.pt</a>
+        </div>
+      </div>
+      <div class="box-footer">
+          <h3 class="contact_social">SIGA-NOS</h3>
+          <div class="text-footer">
+          <a href="https://www.instagram.com/streetdogspt/" target="_blank"><img alt="ícone do Instagram" src="./img/icon/instagram-icon.svg" class="icon-footer size-icon">streetdogspt</a></br>
+          </div>
+          <div class="text-footer">
+            <a href="https://www.facebook.com/streetdogspt/?fref=ts" target="_blank"><img alt="ícone do Facebook" src="./img/icon/facebook.svg" class="icon-footer size-icon">/streetdogspt</a></br>
+          </div>
+      </div>
+      <div class="box-footer MB-text-wrap">
+        <h3 class="contact_social">SIGA-NOS</h3>
+        <div class="MBWay-text">
+        <img alt="ícone do MBWay" src="./img/icon/MBWay.png" class="icon-footer"><a href="tel:+351 968 834 244">968 834 244</a></br></br>
+        </div>
+        <div class="text-footer">
+          <p class="MBWay-text">IBAN PT50 0033 0000 4553078518705</p></br>
+        </div>
+    </div>
+    </div>
+</section>
+
+<!-- Footer -->
+<footer class="footer w3-center">
+  <div class="container">
+    <div class="small text-center text-muted">Copyright &copy; 2019 
+      <a class="link-portfolio" href="https://www.linkedin.com/in/julifortes/" style="text-decoration:none">JULIANA FORTES</a>&nbsp & &nbsp
+      <a class="link-portfolio" href="https://lystrevisan.com/" style="text-decoration:none">LYS TREVISAN</a></div>
+    <div class="small text-center text-muted">All rights reserved</div>
+  </div>
+</footer>
+
+<!-- Custom scripts for this template -->
+<script src="js/script.js"></script>
+<script src="js/creative.min.js"></script>
